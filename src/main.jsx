41e694617f7cc6c5541355b37cc7022840af90e5
Ad1-Sh <113925168+Ad1-Sh@users.noreply.github.com>
2024-05-28 19:31:31 +0530

    Update main.jsx
    removed unnecessary props

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StepSlider from './StepSlider.jsx'
+import Slider from './Slider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StepSlider
-  headerText={'Select Value:'}
-  initialValue={0}
+    <Slider
   minValue={0}
   maxValue={100}
-  stepValue={15}
-  showMinMaxDisplay = {true}
-  showSelectedValueDisplay = {true}
-  showHelpIcon = {true}
-  showAsAmount = {true}
+  stepValue={20}
 />
   </React.StrictMode>,
 )
