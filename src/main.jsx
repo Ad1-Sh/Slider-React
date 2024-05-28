5f2e1305960b18329d942bcecd60c7b87d17d5f5
Ad1-Sh <113925168+Ad1-Sh@users.noreply.github.com>
2024-05-28 16:30:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StepSlider
   headerText={'Select Value:'}
-  headerIcon={'insert icon'}
   initialValue={0}
   minValue={0}
   maxValue={100}
   stepValue={15}
-  leftBoundary={0.15}
-  rightBoundary={0.85}
-  marks={[
-    { value: 14000, label: '₹14,000' },
-    { value: 15000, label: '₹15,000' },
-    { value: 16000, label: '₹16,000' },
-    { value: 17000, label: '₹17,000' },
-    { value: 18000, label: '₹18,000' },
-  ]}
+  showMinMaxDisplay = {true}
+  showSelectedValueDisplay = {true}
+  showHelpIcon = {true}
+  showAsAmount = {true}
 />
   </React.StrictMode>,
 )
